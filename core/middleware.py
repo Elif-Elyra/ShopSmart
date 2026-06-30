009d903caa76f179dfd6e3f0d871247c0adc0158
Nimra5678 <nimrakosar6@gmail.com>
2026-06-30 19:58:17 +0500

    Update the frontend part

--- core/middleware.py
+++ core/middleware.py
@@ -2,25 +2,14 @@ from django.http import HttpResponsePermanentRedirect
 
 
 class LowercaseURLMiddleware:
-
     def __init__(self, get_response):
         self.get_response = get_response
 
     def __call__(self, request):
-
-        path = request.path
-
+        path = request.path_info
+        if path.startswith('/static/') or path.startswith('/media/'):
+            return self.get_response(request)
         if path != path.lower():
-
-            url = path.lower()
-
-            query_string = request.META.get("QUERY_STRING")
-
-            if query_string:
-                url += "?" + query_string
-
-            return HttpResponsePermanentRedirect(url)
-
-        response = self.get_response(request)
-
-        return response
+            new_path = path.lower()
+            return HttpResponsePermanentRedirect(new_path + ('?' + request.META['QUERY_STRING'] if request.META.get('QUERY_STRING') else ''))
+        return self.get_response(request)
